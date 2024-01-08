b6a44aa27d419bc37fcf45baed2709695fa2d7c6
Josh Day <emailjoshday@gmail.com>
2024-01-08 10:59:41 -0500

    readme

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # StringInterp
 
 [![Build Status](https://github.com/joshday/StringInterp.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/joshday/StringInterp.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+
+<p align="center"><b>Speedy customizable string interpolation for Julia.</b></p>
+
+## Usage
+
+```julia
+using StringInterp
+
+# Here's a template.  It looks just like Julia's standard string interpolation syntax.
+t = template"I'm going to interpolate two variables, x and y: $x and $y"
+
+# The thing is that it's lazy.  The String isn't created until you `render` it.
+# Variables are populated via `getproperty`
+render(t, (x=1, y=2))
+
+# Or you can pass variables as keyword args:
+render(t; x=1, y=2)
+
+# Maybe you don't even want to make the string.  You just want to write to an IO:
+render(stdout, t, (x=1, y=2))  # equivalently render(stdout, t; x=1, y=2)
+```
+
+## Advanced Usage with Custom Printers
+
+- You can provide any function to print the interpolated variables (default is `Base.print`).
+- For example, if you're template is based on JSON, you may want to use `JSON3.write` as your printer:
+
+```julia
+using JSON3
+
+t = @template "PlotlyJS.newPlot(\"my_id\", $data, $layout, $config)" printer=JSON3.write
+
+render(t, data=[(; y=rand(2))], layout=(;), config=(; responsive=true))
+```
+
+## Benchmarks
+
+In our benchmark at `benchmarks/suite.jl` we find that **StringInter** is:
+
+- 10.26x faster than [Base string interpolation](https://docs.julialang.org/en/v1/manual/strings/#string-interpolation).
+- 12.17x faster than [Mustache.jl](https://github.com/jverzani/Mustache.jl).
--- benchmarks/suite.jl
+++ benchmarks/suite.jl
@@ -69,6 +69,63 @@ b1 = @benchmark StringInterp.render($t, $obj)
 
 b2 = @benchmark Mustache.render($m, $(Dict(pairs(obj)...)))
 
+b3 = @benchmark let
+    a = 1
+    b = 2
+    c = 3
+    d = 4
+    e = 5
+    f = 6
+    g = 7
+    h = 8
+    i = 9
+    j = 10
+    k = 11
+    l = 12
+    m = 13
+    n = 14
+    o = 15
+    p = 16
+    q = 17
+    r = 18
+    s = 19
+    t = 20
+    u = 21
+    v = 22
+    w = 23
+    x = 24
+    y = 25
+    z = 26
+    """
+    Here is a: $a.
+    Here is b: $b.
+    Here is c: $c.
+    Here is d: $d.
+    Here is e: $e.
+    Here is f: $f.
+    Here is g: $g.
+    Here is h: $h.
+    Here is i: $i.
+    Here is j: $j.
+    Here is k: $k.
+    Here is l: $l.
+    Here is m: $m.
+    Here is n: $n.
+    Here is o: $o.
+    Here is p: $p.
+    Here is q: $q.
+    Here is r: $r.
+    Here is s: $s.
+    Here is t: $t.
+    Here is u: $u.
+    Here is v: $v.
+    Here is w: $w.
+    Here is x: $x.
+    Here is y: $y.
+    Here is z: $z.
+    """
+end
+
 println("#-------------------------------------------------------------------------# StringInterp")
 display(b1)
 
@@ -79,7 +136,14 @@ display(b2)
 
 println("\n\n")
 
+println("#-------------------------------------------------------------------------# Base")
+display(b3)
+
+println("\n\n")
+
 println("#-------------------------------------------------------------------------# Median Ratio")
 r = ratio(median(b2), median(b1))
+r2 = ratio(median(b2), median(b3))
 
 @info "In this benchmark, StringInterp is $(round(r.time, digits=2)) times faster than Mustache."
+@info "In this benchmark, StringInterp is $(round(r2.time, digits=2)) times faster than Base string interpolation."
